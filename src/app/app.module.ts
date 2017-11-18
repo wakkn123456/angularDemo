04650fdf15946478d69c0f1462c252b3d48b786e
wakkn123 <wakkn12@gmail.com>
2017-11-18 15:26:21 +0800

    傳入屬性 – @Input() myProperty; – 在外層元件請記得用「屬性繫結」傳入資料

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ArticleComponent } from './article/article.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
