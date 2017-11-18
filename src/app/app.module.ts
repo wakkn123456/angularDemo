543cf21751dd9704b16b4596d64c6a75e146f622
wakkn123 <wakkn12@gmail.com>
2017-11-18 17:07:35 +0800

    ii

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ArticleComponent } from './article/article.component';
+//import { DataService } from './data.service';
 
 
 @NgModule({
@@ -20,7 +21,9 @@ import { ArticleComponent } from './article/article.component';
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+  //  DataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
